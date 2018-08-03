acceee5d45bbdcf59e2f32a3f0aefe0b22357aaa
D.K <kleinstern.dore@gmail.com>
2018-08-03 21:41:36 +0300

    done 1.1

--- First_Meeting/Matlab/iter.m
+++ First_Meeting/Matlab/iter.m
@@ -5,6 +5,11 @@ change_vec = find(~(H*vec.'-QM)); %find the locations of 'lonely' question marks
 for i = 1:length(change_vec)
     tmp = ~(H(change_vec(i),:).*vec-QM); %find the location of the question mark in the original vector
     out_vec(tmp) = 0; %cheating here, by updating it to be 0 and not XOR of all the rest (since they are allways 0)
+    %% not cheating:
+    %xor_vec = logical(H(change_vec(i),:));
+    %xor_vec(tmp) = 0;
+    %out_vec(tmp) = xor(vec(xor_vec)); %don't know how to do this - xor on
+    %all the items in the array
 end
 
 end
