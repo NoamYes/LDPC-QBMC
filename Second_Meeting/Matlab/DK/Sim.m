d2ecf1e5fd116fc609f1ecf007895d6b6138ea54
D.K <kleinstern.dore@gmail.com>
2018-08-13 22:13:51 +0300

    performance improvement

--- Second_Meeting/Matlab/DK/Sim.m
+++ Second_Meeting/Matlab/DK/Sim.m
@@ -1,18 +1,18 @@
 clc; clear all; close all;
+tic
 %%
 % below will be a code generating graph for erasure rate versus dv/dc as
 % requested
-tic
 
-n = 102; %n
+n = 1002; %n
 k = round(n/2); %k
 QM = 2; %the question mark value > 0
 inc = 0.05;
-tryMat = 2; %how many matrixes to generate for a given epsilon
+tryMat = 100; %how many matrixes to generate for a given epsilon
 tryVec = 100;
 iterLen = 100;
 
-eps_vec = 0:inc:1;
+eps_vec = 0.3:inc:0.6;
 dv = 3; 
 dc = 6;
 
@@ -23,7 +23,7 @@ parfor mat = 1:tryMat
     H(:,:,mat) = Generate_LDCP_H( dv, dc, k, n ); %generate a random H matrix
 end
 
-for idx = 1:numel(eps_vec) %run on epsilon values from 0 to 1 in increments of inc
+parfor idx = 1:numel(eps_vec) %run on epsilon values from 0 to 1 in increments of inc
     eps = eps_vec(idx);
     totalNoise = zeros(tryVec, tryMat);
     mean_mat = zeros(1, tryMat);
@@ -32,8 +32,8 @@ for idx = 1:numel(eps_vec) %run on epsilon values from 0 to 1 in increments of i
             vec = BECnoise(n, eps, QM); %generate a 0 vec with random noise
             totalNoise(j,i) = iter(H(:,:,i), vec, QM, iterLen);
         end
-        disp(round((i/tryMat+idx-1)*100/numel(eps_vec),1)+"% done in " + ...
-            round(toc,1)+" (sec)");
+%         disp(round((i/tryMat+idx-1)*100/numel(eps_vec),1)+"% done in " + ...
+%             round(toc,1)+" (sec)");
     end
     mean_vec(idx) = mean(mean(totalNoise));
 end
@@ -41,7 +41,14 @@ end
 figure(1)
 plot(eps_vec,mean_vec); hold on
 plot(eps_vec,eps_vec);
-title('ratio of erasures after decoding with random noise VS erasure probablity, dc=6,dv=3');
+title('Ratio of erasures after decoding with random noise VS erasure probablity');
 legend('Erasure','y=x')
 xlabel('Probablity Of Erasure [{\epsilon}]');
 ylabel('Erasure Rate');
+
+dim = [.65 .15 .3 .15];
+str = "n = " + n + ", mat# = " + tryMat + ", vec# = " + tryVec + ...
+    ", d_{c} = " + dc + ", d_{v} = " + dv;
+annotation('textbox',dim,'String',str,'FitBoxToText','on');
+
+toc;
--- Second_Meeting/Matlab/DK/iter.m
+++ Second_Meeting/Matlab/DK/iter.m
@@ -3,10 +3,15 @@ function [output] = iter(H, vec, QM, iter_len)
     [row, ~] = size(H);
     Mat = repmat(vec,[row,1]); %initiate a matrix like H with all the repeat
     %of the vector vec
-    var = vec; %initate the var vector
+    old_var = vec; %initate the var vector
     for i = 1:iter_len
-        Mat = var2check(Mat, H, var);
+        Mat = var2check(Mat, H, old_var);
         var = check2var(Mat, QM);
+        if isequal(var, old_var) || ~any(var) %if var didn't change or it 
+            % the 0 vector, end the iteration
+            break;
+        end
+        old_var = var;
     end
     
     new_noise = sum(var(:) == QM);
