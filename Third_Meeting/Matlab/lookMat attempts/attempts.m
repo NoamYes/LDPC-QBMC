e1d973529178880ae37f18abc75c714270034ac6
NoamProg <38458454+NoamProg@users.noreply.github.com>
2018-09-14 02:28:04 +0300

    updated

--- Third_Meeting/Matlab/lookMat attempts/attempts.m
+++ Third_Meeting/Matlab/lookMat attempts/attempts.m
@@ -2,7 +2,7 @@ clear all; clc; close all;
 
 %% nchoosek 
 
-q = 8;
+q = 16;
 k = 8;
 qCell = num2cell(0:q-1);
 subsets = nchoosek(qCell, k); % return all subsets of values 0 to q-1 with size k
@@ -18,3 +18,5 @@ for c = 0:s
     denom = symprod(2^c-2^(b-1), b, 1, c);
     t = t + double(nom)/double(denom);
 end
+
+matSize = t^2*(q-1)^2;
