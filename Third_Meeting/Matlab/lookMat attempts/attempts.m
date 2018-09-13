453590cd7b7d5556aeeba328d96a321975bb9292
NoamProg <38458454+NoamProg@users.noreply.github.com>
2018-09-14 02:17:26 +0300

    added t calculator
    t - number of subgroups calculation for each q

--- Third_Meeting/Matlab/lookMat attempts/attempts.m
+++ Third_Meeting/Matlab/lookMat attempts/attempts.m
@@ -2,7 +2,19 @@ clear all; clc; close all;
 
 %% nchoosek 
 
-q = 16;
+q = 8;
 k = 8;
 qCell = num2cell(0:q-1);
 subsets = nchoosek(qCell, k); % return all subsets of values 0 to q-1 with size k
+
+
+%% size of t
+
+t=0;
+s= log2(q);
+for c = 0:s
+    syms b
+    nom = symprod(2^s-2^(b-1), b, 1, c);
+    denom = symprod(2^c-2^(b-1), b, 1, c);
+    t = t + double(nom)/double(denom);
+end
