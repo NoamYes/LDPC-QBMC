08828043581d502945b7bbcc689ab447d1555eda
D.K <kleinstern.dore@gmail.com>
2019-02-24 21:34:15 +0200

    added rami to git

---
+++ Fourth_Meeting/rami/checkVar.m
@@ -0,0 +1,18 @@
+function [variable] = checkVar(t, q, dv, subsetsLen)
+
+variable = zeros(1, q);
+vec_ndims = dv-1;
+vec_mat_size = t*ones(1,dv-1);
+    for idx_vec = 1:t^(dv-1)
+    %       tic
+            I = cell(1, vec_ndims); 
+            [I{:}] = ind2sub(vec_mat_size,idx_vec);
+            sub_idx_vec = cell2mat(I);
+            H_idx = num2cell(sub_idx_vec);
+            subsetsLen(cell2mat(H_idx))
+            minSize = min(subsetsLen(cell2mat(H_idx)));
+            variable(minSize) = variable(minSize) + 1;
+    %        toc;
+    end
+end
+
