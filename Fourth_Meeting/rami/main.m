24025244fb9fb9117cb3ce99e6e49f0c99f779a7
D.K <kleinstern.dore@gmail.com>
2019-02-25 13:59:06 +0200

    added more code

--- Fourth_Meeting/rami/main.m
+++ Fourth_Meeting/rami/main.m
@@ -19,19 +19,18 @@ q=4;
 % intersectTable = intersect_lookup(subsetTable);
 
 load('q=4_lookups.mat');
-%load('uniform_Pi_Ii_4.mat');
 
 %% PI CAL
 
 t = length(subsetTable);
 subsetsLen = cellfun('length', subsetTable);
 lenIdxsTable = cell(1,q);
-Pi_len_cell = cell(1,q);
+size_distribution_cell = cell(1,q);
 for len = 1:q
-    [~,lenIdxsTable{len}] = find(len >= subsetsLen); %% DK: I FLIPPED THESE TWO
+    [~,lenIdxsTable{len}] = find(len >= subsetsLen);
     pi = zeros(1,t);
     pi(lenIdxsTable{len}) = 1/numel(lenIdxsTable{len});
-    Pi_len_cell{len} = pi;
+    size_distribution_cell{len} = pi;
 end
 
 
@@ -63,11 +62,11 @@ for idx = 1:numel(e1_vec) %run on epsilon values from 0 to 1 in increments of in
             mean_mat(idx,jdx) = 1;
         else
             e_vec = [(1-e1-e2) e1 e2];
-            [Z] = EquationDecoding2(e_vec , Pi_len_cell, IiMat, t, q, dc, looktable, dividetable, subsetsLen);
+            [Z] = EquationDecoding2(e_vec , size_distribution_cell, IiMat, t, q, dc, looktable, dividetable, subsetsLen);
             mean_mat(idx,jdx) = 1 - Z(1);
         end
     end
-     disp(round(idx/numel(e1_vec)*100,1)+"% done in "+round(toc,1)+" (sec)");
+    disp(round(idx/numel(e1_vec)*100,1)+"% done in "+round(toc,1)+" (sec)");
 end
 figure(1)
 imshow(mean_mat.', 'XData', e2_vec, 'YData', e1_vec);
