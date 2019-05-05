6c18797983e4d94e5bbd4170caff0238624842bd
Dore Kleinstern <dore@ef.technion.ac.il>
2019-05-05 14:24:21 +0300

    did nothing

--- Sixth Meeting/BallsandBins.m
+++ Sixth Meeting/BallsandBins.m
@@ -7,20 +7,20 @@ function [Pi] = BallsandBins(q,sizeArray)
 diagonal = (0:q)/q;
 tranMat = diag(diagonal);
 tranMat(2:q+2:end)=1-(0:q-1)/q;
-tranMat = tranMat.';
+tranMat = tranMat.'; % markov matrix
 Pi =  1:log2(q)+1;
 g_zero = zeros(1,q+1);
 g_zero(1)=1;
 
-N = prod(sizeArray,'all');
+N = prod(sizeArray);
 g_N = g_zero*(tranMat)^N;
 
 indices = 1:numel(g_N);
 powers = ceil(log2(indices-1));
 powers2 = 2.^(0:log2(q));
-for i=0:log2(q)
+for i=1:log2(q)+1
     %    Pi(i+1) = sum(g_N(powers == i));
-    Pi(i+1) = g_N(powers2(i+1)+1);
+    Pi(i) = g_N(powers2(i)+1);
 end
 Pi = Pi/sum(Pi);
 
--- Sixth Meeting/EquationDecoding.m
+++ Sixth Meeting/EquationDecoding.m
@@ -3,7 +3,7 @@ function [Z] = EquationDecoding(e_vec, q, dc, dv, PiMat)
 Z = e_vec;
 iterLen = 100;
     for iter = 1:iterLen
-
+        
         W = w_approx(Z, q, dc, PiMat);
         Z = z_approx(W, q, dv, e_vec);
         Z = Z/sum(Z);
