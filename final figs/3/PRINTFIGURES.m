014c3cbdd3790e9bcb50edda33ac83e89c9d1024
NoamProg <38458454+NoamProg@users.noreply.github.com>
2019-08-19 17:00:05 +0300

    updates

---
+++ final figs/3/PRINTFIGURES.m
@@ -0,0 +1,11 @@
+for i=1:2
+    figure(i)
+    set(gcf, 'Position',  [1690.333333333333,555,565.9999999999998,552.6666666666665])
+    
+end
+
+
+figure(1)
+title('total erasure rate for q=4, n=2004, uniform label distribution');
+figure(2)
+title('total erasure rate for q=4, n=2004, uniform 2 labels distribution');
---
+++ final figs/4/PRINTFIGURES.m
@@ -0,0 +1,12 @@
+for i=1:3
+    figure(i)
+    set(gcf, 'Position',  [1690.333333333333,555,565.9999999999998,552.6666666666665])
+    
+end
+
+% figure(1)
+% title('total erasure rate for q=4, n=2004, uniform label distribution');
+figure(2)
+title('total erasure rate for q=4, density evolution, uniform label distribution');
+figure(3)
+title('total erasure rate for q=4, density evolution, uniform 2 labels distribution');
